2dfe85f983a63492c56ed4481ff65493536bc6ef
lumiknit <aasr4r4@gmail.com>
2018-12-28 20:56:51 +0900

    Changed all images should be save into the internal memory Changed to save image lists

--- app/src/main/java/io/madcamp/yh/mc_assignment1/Tab2Adapter.java
+++ app/src/main/java/io/madcamp/yh/mc_assignment1/Tab2Adapter.java
@@ -1,8 +1,13 @@
 package io.madcamp.yh.mc_assignment1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Tab2Adapter extends RecyclerView.Adapter<Tab2Adapter.ImageViewHolder> {
@@ -22,16 +28,53 @@ public class Tab2Adapter extends RecyclerView.Adapter<Tab2Adapter.ImageViewHolde
             imageView = itemView.findViewById(R.id.image_view);
             textView = itemView.findViewById(R.id.text_view);
         }
+
+        public void setBitmap(Uri uri) {
+            imageView.setImageURI(uri);
+            /* Bitmap bm = postprocessOfCamera(uri);
+            if(bm != null) {
+                imageView.setImageBitmap(bm);
+            } */
+        }
+
+        private Bitmap postprocessOfCamera(Uri uri) {
+            /* try {
+                Bitmap image = BitmapFactory.decodeStream(imageView.getContext().getContentResolver().openInputStream(uri));
+                ExifInterface ei = new ExifInterface(imageView.getContext().getContentResolver().openInputStream(uri));
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+                Bitmap rotatedBitmap = null;
+                float angle = 0.0f;
+                switch(orientation) {
+                    case ExifInterface.ORIENTATION_ROTATE_90: angle = 90.f; break;
+                    case ExifInterface.ORIENTATION_ROTATE_180: angle = 180.f; break;
+                    case ExifInterface.ORIENTATION_ROTATE_270: angle = 270.f; break;
+                }
+                Log.d("Angle", "" + angle);
+                Bitmap rotated = image;
+                if(angle != 0.0f) rotated = rotateImage(image, angle);
+                return rotated;
+            } catch(IOException e) {
+                e.printStackTrace();
+                return null;
+            } */ return null;
+        }
+
+        private static Bitmap rotateImage(Bitmap source, float angle) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(angle);
+            return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+        }
     }
 
-    public ArrayList<Pair<String, String>> dataset;
+    public ArrayList<Pair<Uri, String>> dataset;
 
-    public Tab2Adapter(ArrayList<Pair<String, String>> dataset) {
+    public Tab2Adapter(ArrayList<Pair<Uri, String>> dataset) {
         this.dataset = dataset;
     }
 
-    public void add(String uri, String name) {
-        dataset.add(new Pair<>(uri, name));
+    public void add(Uri uri, String tag) {
+        dataset.add(new Pair<>(uri, tag));
         notifyDataSetChanged();
     }
 
@@ -52,8 +95,8 @@ public class Tab2Adapter extends RecyclerView.Adapter<Tab2Adapter.ImageViewHolde
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder viewHolder, int i) {
         ImageViewHolder vh = (ImageViewHolder)viewHolder;
-        Pair<String, String> p = dataset.get(i);
-        vh.imageView.setImageURI(Uri.parse(p.first));
+        Pair<Uri, String> p = dataset.get(i);
+        vh.setBitmap(p.first);
         vh.textView.setText(p.second);
     }
 
--- app/src/main/java/io/madcamp/yh/mc_assignment1/Tab2Fragment.java
+++ app/src/main/java/io/madcamp/yh/mc_assignment1/Tab2Fragment.java
@@ -1,24 +1,36 @@
 package io.madcamp.yh.mc_assignment1;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 
+import org.json.*;
+
+import java.io.*;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Tab2Fragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
@@ -33,6 +45,8 @@ public class Tab2Fragment extends Fragment {
 
     private RecyclerView recyclerView;
     private Tab2Adapter adapter;
+
+    private Uri tempPhotoUri;
 
     /* TabPagerAdapter에서 Fragment 생성할 때 사용하는 메소드 */
     public static Tab2Fragment newInstance(int page) {
@@ -58,8 +72,13 @@ public class Tab2Fragment extends Fragment {
         initializeFloatingActionButton();
         initializeRecyclerView();
 
-
         return top;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        saveImageListToInternal();
     }
 
     public void initializeFloatingActionButton() {
@@ -115,9 +134,84 @@ public class Tab2Fragment extends Fragment {
         fab2.setOnClickListener(onClickListener);
     }
 
+    final static String listPath = "images.json";
+
+    public void saveImageListToInternal() {
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(listPath, Context.MODE_PRIVATE);
+            String res = packJSON();
+            Log.d("saveImageListToInternal", res);
+            fos.write(res.getBytes());
+            fos.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Pair<Uri, String>> loadImageListFromInternal() {
+        ArrayList<Pair<Uri, String>> list = new ArrayList<>();
+        FileInputStream fis;
+        InputStreamReader isr;
+        BufferedReader br;
+        StringBuilder sb;
+        String line;
+        try {
+            fis = context.openFileInput(listPath);
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            sb = new StringBuilder();
+            while((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            fis.close();
+            String src = sb.toString();
+            Log.d("loadImageListInternal", src);
+            unpackJSON(src, list);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private String packJSON() {
+        try {
+            JSONArray arr = new JSONArray();
+            ArrayList<Pair<Uri, String>> list = adapter.dataset;
+            if(list != null) {
+                for (Pair<Uri, String> p : list) {
+                    JSONObject item = new JSONObject();
+                    String path = p.first.getPath();
+                    Log.d("path", path);
+                    item.put("uri", path);
+                    item.put("tag", p.second.toString());
+                    arr.put(item);
+                }
+            }
+            return arr.toString();
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void unpackJSON(String src, ArrayList<Pair<Uri, String>> list) {
+        try {
+            JSONArray arr = new JSONArray(src);
+            for(int i = 0; i < arr.length(); i++) {
+                JSONObject item = arr.getJSONObject(i);
+                Uri uri = Uri.parse(item.getString("uri"));
+                String tag = item.getString("tag");
+                list.add(new Pair<>(uri, tag));
+            }
+        } catch(JSONException e) {
+            Log.d("Exception", "JSON Parsing Failed");
+        }
+    }
+
     public void initializeRecyclerView() {
         recyclerView = (RecyclerView)top.findViewById(R.id.recycler_view);
-        adapter = new Tab2Adapter(new ArrayList<Pair<String, String>>());
+        adapter = new Tab2Adapter(loadImageListFromInternal());
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(context, 4));
         recyclerView.setAdapter(adapter);
@@ -130,26 +224,76 @@ public class Tab2Fragment extends Fragment {
     }
 
     public void addImageFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(context.getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = timeStamp;
+                File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            if(photoFile != null) {
+                tempPhotoUri = FileProvider.getUriForFile(context, "io.madcamp.yh.mc_assignment1.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, tempPhotoUri);
+                startActivityForResult(intent, REQ_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private static String decodeFilename(Uri uri) {
+        try {
+            String path = URLDecoder.decode(uri.toString(), "UTF-8");
+            String filename = path.substring(path.lastIndexOf("/") + 1);
+            if(filename.length() < 1) filename = "noname";
+            return filename;
+
+        } catch(Exception e) {
+            return "noname";
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == Activity.RESULT_OK) {
+        if(resultCode == Activity.RESULT_OK && (requestCode == REQ_IMG_FILE || requestCode == REQ_TAKE_PHOTO)) {
+            Uri uri = null;
+            String filename = null;
             switch(requestCode) {
                 case REQ_IMG_FILE:
-                    Uri uri = data.getData();
-                    try {
-                        String path = URLDecoder.decode(uri.toString(), "UTF-8");
-                        String filename = path.substring(path.lastIndexOf("/") + 1);
-                        if(filename.length() < 1) filename = "NONAME";
-                        adapter.add(uri.toString(), filename);
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    }
+                    uri = data.getData();
+                    filename = decodeFilename(uri);
                     break;
                 case REQ_TAKE_PHOTO:
+                    uri = tempPhotoUri;
+                    filename = decodeFilename(tempPhotoUri);
                     break;
             }
+
+            Uri newUri = copyToInternal(uri);
+            adapter.add(newUri, filename);
+        }
+    }
+
+    private Uri copyToInternal(Uri uri) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = timeStamp + ".jpg";
+        try {
+            String newDir = context.getFilesDir().toString();
+            File fout = new File(newDir, "images");
+            fout.mkdir();
+
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+            FileOutputStream fos = context.openFileOutput(imageFileName, Context.MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.close();
+            Uri newUri = Uri.parse(context.getFileStreamPath(imageFileName).getAbsolutePath());
+            Log.d("newUri", newUri.getPath());
+            return newUri;
+        } catch(IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
